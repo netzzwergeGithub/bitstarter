986a97330c904970ef6d1c144342e8194702ead5
Martin Dehnert <startup@netzzwerge.de>
2013-07-05 21:38:27 +0000

    web.js reads fro, file

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require ('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var indexBuffer = fs.readFileSync("index.html");
+var indexText = indexBuffer.toString();  
+response.send(indexText );
 });
 
 var port = process.env.PORT || 5000;
